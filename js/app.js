1206ff4ce7bc71d0fe7f097c1b1b40d50fafb26c
Chris Garvin <chris.garvin@me.com>
2016-01-07 21:49:56 -0800

    added skeleton design and json

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+$.getJSON("data/data.json", function(data) {
 
+  $('.charles img').mouseenter(function(){
+    $('.charles span')[0].innerHTML = "<div><h3>" + data.response.info[1].name + "</h3><br>" + data.response.info[1].job + "<br>" + data.response.info[1].twitter + "</div>";
+  })
+
+  $('.charles img').mouseleave(function(){
+    $('.charles span')[0].innerHTML = "";
+  })
+
+  $('.chris img').mouseenter(function(){
+    $('.chris span')[0].innerHTML = "<div><h3>" + data.response.info[0].name + "</h3><br>" + data.response.info[0].job + "<br>" + data.response.info[0].twitter + "</div>";
+  })
+
+  $('.chris img').mouseleave(function(){
+    $('.chris span')[0].innerHTML = "";
+  })
+
+  });
 
 });
